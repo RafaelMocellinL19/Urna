b164104b2a9e4483c50858f883322aa9c750b66c
Rafael Mocellin <rafael.mocellin.leszczynski@gmail.com>
2022-07-20 20:16:50 -0300

    commit inicial

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR:',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: ': Fulaninho Santos',
+                partido: ': SQL',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Governador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: ': Beltrana da Silva',
+                partido: ': JAVA',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Governador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE:',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: ': João Doe S.',
+                partido: ': PHP',
+                vice: ': Gen.M',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Presidente'},
+                    {url:'99_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: ': Ciclano Motta ',
+                partido: ': VPN',
+                vice: ': D. Meta',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Presidente'},
+                    {url:'84_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+        ]
+    }
+];
